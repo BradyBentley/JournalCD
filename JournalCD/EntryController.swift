d24a44d0bb0771706eec6846233e4ad7905f406a
Brady Bentley <bradybentley@Bradys-MacBook-Pro.local>
2018-12-06 12:11:28 -0700

    Adds fetched results data to allow the data to find data

--- JournalCD/EntryController.swift
+++ JournalCD/EntryController.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import CoreData
+
+class EntryController {
+    
+    // MARK: - Properties
+    
+    //shared Instance AKA Singleton
+    static let shared = EntryController()
+    private init (){}
+    
+    // MARK: - CRUD Functions
+    func addEntryWith(title: String, body: String){
+        Entry(title: title, body: body)
+        saveToPersistentStore()
+    }
+    
+    func update(entry: Entry, newTitle: String, newBody: String){
+        entry.title = newTitle
+        entry.body = newBody
+        saveToPersistentStore()
+    }
+    
+    func delete(entry: Entry){
+        //asks parent what sand box it is living in and delete it from there
+        entry.managedObjectContext?.delete(entry)
+        saveToPersistentStore()
+    }
+    
+    // MARK: - PERSISTENCE
+    
+    func saveToPersistentStore(){
+        do {
+            try CoreDataStack.context.save()
+        } catch {
+            print("There was an error in \(#function) : \(error.localizedDescription)")
+        }
+    }
+}

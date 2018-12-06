d302bf924510ccf296ee5fd6c2ef51615f00a369
Brady Bentley <bradybentley@Bradys-MacBook-Pro.local>
2018-12-06 11:25:58 -0700

    Adds convenience initializer for entry model

---
+++ JournalCD/Model/CoreDataStack.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataStack.swift
+//  JournalCD
+//
+//  Created by Brady Bentley on 12/6/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    static let container: NSPersistentContainer = {
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
+
---
+++ JournalCD/Model/Entry+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  Entry+Convenience.swift
+//  JournalCD
+//
+//  Created by Brady Bentley on 12/6/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    
+    @discardableResult
+    convenience init(title: String, body: String, timestamp: Date = Date(), context: NSManagedObjectContext = CoreDataStack.context){
+        self.init(context: context)
+        self.title = title
+        self.body = body
+        self.timeStamp = timestamp
+    }
+    
+}
